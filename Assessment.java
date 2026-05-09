a91072a885cc44543b238ef74e4cddabd3107ab6
MonkeyNoChunky <oweefo@gmail.com>
2026-05-09 17:36:13 -0400

    worked on architecture
    decided that the differences between tests, exams, and assignemtns will be based on the weights that the teacher will assign before creating the subject. Because tests, exams, and assignments dont have different attributes, I am removing those classes.

--- Assessment.java
+++ Assessment.java
@@ -1,16 +1,19 @@
-public abstract class Assessment {
-    //fields
-    //constructor
-    public static class Assignment extends Assessment {
-        //fields
-        //constructor
+public class Assessment {
+    private String name;
+    private double score;
+    private double maxScore;
+    private double weight;
+    private String type; // either "exam", "test", or "assignment"
+
+    public Assessment(String name, double score, double maxScore, double weight, String type) {
+        this.name = name;
+        this.score = score;
+        this.maxScore = maxScore;
+        this.weight = weight;
+        this.type = type;
     }
-    public static class Exam extends Assessment {
-        //fields
-        //constructor
-    }           
-    public static class Test extends Assessment {
-        //fields
-        //constructor
+
+    public String getType() {
+        return type;
     }
 }
--- Subject.java
+++ Subject.java
@@ -2,5 +2,29 @@ import java.util.ArrayList;
 
 public class Subject {
     private String name;
-    private ArrayList<Assessment> assessments;
+    private int testsWeight;
+    private int assignmentsWeight;
+    private int examWeight;
+    private ArrayList<Assessment> tests;
+    private ArrayList<Assessment> assignments;
+    private Assessment exam; 
+
+    public Subject(String name, int testsWeight, int assignmentsWeight, int examWeight) {
+        this.name = name;
+        this.testsWeight = testsWeight;
+        this.assignmentsWeight = assignmentsWeight;
+        this.examWeight = examWeight;
+    }
+
+    public void addAssessment(Assessment assessment) {
+        if(assessment.getType().equals("exam")) {
+            exam = assessment;
+        }
+        if(assessment.getType().equals("test")) {
+            tests.add(assessment);
+        }
+        if(assessment.getType().equals("assignment")) {
+            assignments.add(assessment);
+        }
+    }
 }
